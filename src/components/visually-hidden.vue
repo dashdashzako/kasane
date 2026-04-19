6426a9c5780f3c293dff6ccbe4b2790f30bb3182
Damien Caselli <damien.caselli@gmail.com>
2026-04-19 17:53:18 +0200

    feat: add visually hidden component

---
+++ src/components/visually-hidden.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { tag = 'span' } = defineProps<{ tag?: string }>()
+</script>
+
+<template>
+  <component :is="tag" class="visually-hidden"><slot /></component>
+</template>
+
+<style lang="css" scoped>
+.visually-hidden {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+</style>
