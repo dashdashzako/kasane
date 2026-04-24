d4601d966e609121a2bb7ac8944e50114fc4b613
Damien Caselli <damien.caselli@gmail.com>
2026-04-24 23:53:49 +0200

    build: add release and deploy process

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.CI ? '/kasane/' : '/',
   plugins: [vueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
