4f24349cb38a075edc76911bd194a0a1a506403f
Damien Caselli <damien.caselli@gmail.com>
2026-05-01 16:46:02 +0200

    feat: add transition between index and color pages

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
   <main>
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </main>
 </template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+</style>
